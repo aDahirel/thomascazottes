051e2320c639723164fd622d8f51f7d3e12cd319
aDahirel <axel.dahirel@gmail.com>
2019-09-19 18:36:40 +0200

    Ajout du premier controller, d'un premier template et des différents composants pré-requis
    Ajout du premier 'Controller',  import en ligne de code de Annotations et Twig, création d'un premier template twig pour vérifier l'affichage.

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/MainController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+}
